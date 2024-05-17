43ea4000c3285ae3c2a00c6c1c48cab16d418cc2
wjdgur778 <wjdgur778@gmail.com>
2024-05-17 14:10:48 +0900

    bj 1764

---
+++ backjoon/Main1764/Main.java
@@ -0,0 +1,37 @@
+package backjoon.Main1764;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int cnt = 0;
+        ArrayList<String> arr = new ArrayList<>();
+        HashSet<String> set = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+                set.add(br.readLine());
+        }
+        for (int i = 0; i < M; i++) {
+            String tmp = br.readLine();
+            if(set.contains(tmp)){
+                cnt++;
+                arr.add(tmp);
+            }
+        }
+        Collections.sort(arr);
+        System.out.println(cnt);
+        for(String str : arr){
+            System.out.println(str);
+        }
+
+    }
+}
